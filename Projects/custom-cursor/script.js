d8e1795ffd2575d6d68caeafca34958cca01ed4c
Anshul Chauhan <167734958+anshul-chauhan2024@users.noreply.github.com>
2025-03-29 18:16:40 +0530

    Delete Projects directory

--- Projects/custom-cursor/script.js
+++
@@ -1,8 +0,0 @@
-let parent = document.querySelector('.parent');
-let cursor = document.querySelector('.cursor');
-
-parent.addEventListener('mousemove', (val) => {
-    cursor.style.opacity = 1;
-    cursor.style.left = val.x + 'px';
-    cursor.style.top = val.y + 'px';
-})
